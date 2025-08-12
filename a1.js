2cbcb69432bdd1f05f6bf870937f8d61c02573d0
NectoAnon <hlebihec@gmail.com>
2025-08-12 12:21:43 +0300

    Add files via upload

---
+++ a1.js
@@ -0,0 +1,18 @@
+const webApp = Telegram.WebApp;
+
+        // Инициализация WebApp
+        webApp.expand(); // Раскрываем на весь экран
+        webApp.enableClosingConfirmation(); // Запрос подтверждения при закрытии
+
+        document.getElementById('sendBtn').addEventListener('click', () => {
+            // Отправляем данные в бота (они придут в web_app_data)
+            webApp.sendData("Привет, это сообщение из WebApp!");
+            
+            // Альтернатива: закрыть WebApp с передачей данных
+            // webApp.close(); // Если нужно просто закрыть
+        });
+
+        // Обработчик события закрытия (опционально)
+        webApp.onEvent('closeButtonClicked', () => {
+            console.log('WebApp закрывается');
+        });
