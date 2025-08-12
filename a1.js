7abee4cd237e9e1c5a8554ea1cda2a0c2f3558b9
NectoAnon <hlebihec@gmail.com>
2025-08-13 01:54:15 +0300

    Update a1.js

--- a1.js
+++ a1.js
@@ -11,14 +11,15 @@ let game_1 = document.getElementById("game_1");
 
 game_1.addEventListener("click", function(){
   tg.sendData("some string that we need to send");
-  item = "1";
+  window.Telegram.WebApp.sendData('ijiiik');
  });
 
 
 
- /*window.Telegram.WebApp.sendData(item);  */
+ /*window.Telegram.WebApp.sendData('ijiiik');  */
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
- tg.sendData(item);
+ /*tg.sendData(item);*/
+ tg.sendData("some string that we need to send");
+ window.Telegram.WebApp.sendData('ijiiik');
 });
-
