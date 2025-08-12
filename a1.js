2c503837b64b495cabf104480c837d0e606b8300
NectoAnon <hlebihec@gmail.com>
2025-08-12 12:50:13 +0300

    Update a1.js

--- a1.js
+++ a1.js
@@ -1,19 +1,3 @@
-const webApp = Telegram.WebApp;
+var WebApp = window.Telegram.WebApp;
 
-        // Инициализация WebApp
-        webApp.expand(); // Раскрываем на весь экран
-        webApp.enableClosingConfirmation(); // Запрос подтверждения при закрытии
-
-        document.getElementById('game_1').addEventListener('click', () => {
-            // Отправляем данные в бота (они придут в web_app_data)
-            webApp.sendData("Привет, это сообщение из WebApp!");
-            
-            // Альтернатива: закрыть WebApp с передачей данных
-            // webApp.close(); // Если нужно просто закрыть
-        });
-
-        // Обработчик события закрытия (опционально)
-        webApp.onEvent('closeButtonClicked', () => {
-            console.log('WebApp закрывается');
-
-        });
+WebApp.showAlert('Даров брат');
