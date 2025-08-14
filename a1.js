fe0f5f84f2fea60eca9721669dc08e2646cae6b2
NectoAnon <hlebihec@gmail.com>
2025-08-14 14:17:46 +0300

    Update a1.js

--- a1.js
+++ a1.js
@@ -1,15 +1,20 @@
-document.getElementById('game_1').addEventListener('click', () => {
-            fetch('/send-message', {
-                method: 'POST'
-            })
-            .then(response => response.text())
-            .then(data => {
-                console.log(data);
-                alert('Сообщение отправлено!');
-            })
-            .catch(error => {
-                console.error('Ошибка:', error);
-            });
-        });
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+
+
+let item = "";
+
+let game_1 = document.getElementById("game_1");
+
+
+game_1.addEventListener("click", function(){
+  tg.sendData("some string that we need to send");
+  console.log(data);
+  alert('Ну что то отправилось ');
+ });
+
+
 
 
