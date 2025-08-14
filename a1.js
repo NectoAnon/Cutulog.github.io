dd36a69c7aace5d157402a41fea65dddf304e520
NectoAnon <hlebihec@gmail.com>
2025-08-14 14:32:40 +0300

    Update a1.js

--- a1.js
+++ a1.js
@@ -11,9 +11,9 @@ let game_1 = document.getElementById("game_1");
 
 game_1.addEventListener("click", function(){
   tg.sendData("some string that we need to send");
-  console.log(data);
-  alert('Ну что то отправилось ');
+  
  });
+
 
 
 
